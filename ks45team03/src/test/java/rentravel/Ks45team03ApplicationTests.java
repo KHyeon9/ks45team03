22bcfb1549b22be4ebbdda546dafd18359d86316
강 현 구 <mrk19967@gmail.com>
2022-12-23 12:03:46 +0900

    [ks45team03] 프로젝트 생성

---
+++ ks45team03/src/test/java/rentravel/Ks45team03ApplicationTests.java
@@ -0,0 +1,13 @@
+package rentravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ks45team03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
