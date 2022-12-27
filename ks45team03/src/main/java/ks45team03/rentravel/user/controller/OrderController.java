ca79a0c702dd281df030ee568fbd07be379ef4d7
ksmart <ksmart@DESKTOP-LIJ9H19>
2022-12-27 12:37:51 +0900

    [강현구] order의 userPayment 생성 및 main에 a태그로 연결

--- ks45team03/src/main/java/ks45team03/rentravel/user/controller/OrderController.java
+++ ks45team03/src/main/java/ks45team03/rentravel/user/controller/OrderController.java
@@ -1,8 +1,18 @@
 package ks45team03.rentravel.user.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/order")
 public class OrderController {
 	
+	@GetMapping("/userPayment")
+	public String payment(Model model) {
+		model.addAttribute("title", "결제 화면");
+		
+		return "user/order/userPayment";
+	}
 }
