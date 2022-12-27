4db65a686ca58e2b19ab9d18c156391faf2fa7cb
toroy <toroy@DESKTOP-32BBT50>
2022-12-27 16:37:56 +0900

    [221227 김대현] UserController 리퀘스트매핑 삭제 MyPageController 내정보, 수정, 삭제 생성 mypage.html을 userInfo와 연결 modifyUser.html, removeUser.html 생성

--- ks45team03/src/main/java/ks45team03/rentravel/user/controller/MyPageController.java
+++ ks45team03/src/main/java/ks45team03/rentravel/user/controller/MyPageController.java
@@ -3,29 +3,43 @@ package ks45team03.rentravel.user.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/user/myPage")
 public class MyPageController {
 	
-	@GetMapping("/user/myPage/myPage")
-	public String myPage(Model model) {
-		model.addAttribute("title","마이페이지 화면");
+	@GetMapping("/myInfo")
+	private String myInfo() {
+		
 		return "user/myPage/myPage";
 	}
 	
-	@GetMapping("/user/myPage/myGoodsList")
+	@GetMapping("/modifyUser")
+	private String modifyUser() {
+		
+		return "user/myPage/modifyUser";
+	}
+	
+	@GetMapping("/removeUser")
+	private String removeUser() {
+		
+		return "user/myPage/removeUser";
+	}
+	
+	@GetMapping("/myGoodsList")
 	public String myGoodsList(Model model) {
 		model.addAttribute("title","마이페이지 화면");
 		return "user/myPage/myGoodsList";
 	}
 	
-	@GetMapping("/user/myPage/myReviewList")
+	@GetMapping("/myReviewList")
 	public String myReviewList(Model model) {
 		model.addAttribute("title","마이페이지 화면");
 		return "user/myPage/myReviewList";
 	}
 	
-	@GetMapping("/user/myPage/myWishList")
+	@GetMapping("/myWishList")
 	public String myWishList(Model model) {
 		model.addAttribute("title","마이페이지 화면");
 		return "user/myPage/myWishList";
