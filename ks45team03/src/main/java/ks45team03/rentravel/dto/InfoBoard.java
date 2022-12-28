a98ccef16adba9190522cb277a1dc17dd86cfa42
ksmart <ksmart@DESKTOP-LIJ9H19>
2022-12-28 17:20:15 +0900

    [221228 강현구] 정보게시판 리스트 추가

--- ks45team03/src/main/java/ks45team03/rentravel/dto/InfoBoard.java
+++ ks45team03/src/main/java/ks45team03/rentravel/dto/InfoBoard.java
@@ -9,56 +9,107 @@ public class InfoBoard {
 	private int infoBoardViewCnt;
 	private String infoBoardRegTime;
 	private User user;
+
 	
 	public String getInfoBoardCode() {
 		return infoBoardCode;
 	}
+
+
 	public void setInfoBoardCode(String infoBoardCode) {
 		this.infoBoardCode = infoBoardCode;
 	}
+
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getInfoBoardTitle() {
 		return infoBoardTitle;
 	}
+
 	public void setInfoBoardTitle(String infoBoardTitle) {
 		this.infoBoardTitle = infoBoardTitle;
 	}
+
 	public String getInfoBoardContent() {
 		return infoBoardContent;
 	}
+
 	public void setInfoBoardContent(String infoBoardContent) {
 		this.infoBoardContent = infoBoardContent;
 	}
-	public int getInfoBoardLikeCount() {
+
+	public int getInfoBoardLikeCnt() {
 		return infoBoardLikeCnt;
 	}
-	public void setInfoBoardLikeCount(int infoBoardLikeCount) {
-		this.infoBoardLikeCnt = infoBoardLikeCount;
+
+	public void setInfoBoardLikeCnt(int infoBoardLikeCnt) {
+		this.infoBoardLikeCnt = infoBoardLikeCnt;
 	}
+
 	public int getInfoBoardViewCnt() {
 		return infoBoardViewCnt;
 	}
+
 	public void setInfoBoardViewCnt(int infoBoardViewCnt) {
 		this.infoBoardViewCnt = infoBoardViewCnt;
 	}
+
 	public String getInfoBoardRegTime() {
 		return infoBoardRegTime;
 	}
+
+
+
+
 	public void setInfoBoardRegTime(String infoBoardRegTime) {
 		this.infoBoardRegTime = infoBoardRegTime;
 	}
+
+
+
+
 	public User getUser() {
 		return user;
 	}
+
+
+
+
 	public void setUser(User user) {
 		this.user = user;
 	}
-	
-	
+
+
+
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("InfoBoard [infoBoardCode=");
+		builder.append(infoBoardCode);
+		builder.append(", userId=");
+		builder.append(userId);
+		builder.append(", infoBoardTitle=");
+		builder.append(infoBoardTitle);
+		builder.append(", infoBoardContent=");
+		builder.append(infoBoardContent);
+		builder.append(", infoBoardLikeCnt=");
+		builder.append(infoBoardLikeCnt);
+		builder.append(", infoBoardViewCnt=");
+		builder.append(infoBoardViewCnt);
+		builder.append(", infoBoardRegTime=");
+		builder.append(infoBoardRegTime);
+		builder.append(", user=");
+		builder.append(user);
+		builder.append("]");
+		return builder.toString();
+	}
 	
 }
