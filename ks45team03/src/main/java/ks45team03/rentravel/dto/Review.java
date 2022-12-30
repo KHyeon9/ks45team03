d5b98ca46acb4a68cfdbb6bcc5777825dfc42d5d
ASUS <ASUS@DESKTOP-T50VO8O>
2022-12-30 17:24:05 +0900

    [221230 남윤희] goodsList, addGoods, modifyGoods 수정 goodsList, addGoods, modifyGoods 작업중

--- ks45team03/src/main/java/ks45team03/rentravel/dto/Review.java
+++ ks45team03/src/main/java/ks45team03/rentravel/dto/Review.java
@@ -11,61 +11,88 @@ public class Review {
 	private String mileageGroupCode;
 	private String saveMileage;
 	
+	private String goodsName;
+
 	public String getReviewCode() {
 		return reviewCode;
 	}
+
 	public void setReviewCode(String reviewCode) {
 		this.reviewCode = reviewCode;
 	}
+
 	public String getGoodsCode() {
 		return goodsCode;
 	}
+
 	public void setGoodsCode(String goodsCode) {
 		this.goodsCode = goodsCode;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getReviewContent() {
 		return reviewContent;
 	}
+
 	public void setReviewContent(String reviewContent) {
 		this.reviewContent = reviewContent;
 	}
+
 	public int getGoodsEvaluation() {
 		return goodsEvaluation;
 	}
+
 	public void setGoodsEvaluation(int goodsEvaluation) {
 		this.goodsEvaluation = goodsEvaluation;
 	}
+
 	public String getReviewImg() {
 		return reviewImg;
 	}
+
 	public void setReviewImg(String reviewImg) {
 		this.reviewImg = reviewImg;
 	}
+
 	public String getReviewRegDate() {
 		return reviewRegDate;
 	}
+
 	public void setReviewRegDate(String reviewRegDate) {
 		this.reviewRegDate = reviewRegDate;
 	}
+
 	public String getMileageGroupCode() {
 		return mileageGroupCode;
 	}
+
 	public void setMileageGroupCode(String mileageGroupCode) {
 		this.mileageGroupCode = mileageGroupCode;
 	}
+
 	public String getSaveMileage() {
 		return saveMileage;
 	}
+
 	public void setSaveMileage(String saveMileage) {
 		this.saveMileage = saveMileage;
 	}
-	
+
+	public String getGoodsName() {
+		return goodsName;
+	}
+
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -87,6 +114,8 @@ public class Review {
 		builder.append(mileageGroupCode);
 		builder.append(", saveMileage=");
 		builder.append(saveMileage);
+		builder.append(", goodsName=");
+		builder.append(goodsName);
 		builder.append("]");
 		return builder.toString();
 	}
