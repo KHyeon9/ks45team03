5d043d15b25d478f43a1d8fc6d2a12d9a71070f9
toroy <toroy@DESKTOP-32BBT50>
2022-12-30 13:36:04 +0900

    [221230 김대현] 로그인 메인화면 모달창으로 구현 아이디 비밀번호 추후 구현 아이디 비밀번호 틀렸을시 리다이렉트 후 모달창이 자동으로 뜨게 구현했는데 부트스트랩 메인 로딩이 거슬림.

--- ks45team03/src/main/java/ks45team03/rentravel/mapper/UserMapper.java
+++ ks45team03/src/main/java/ks45team03/rentravel/mapper/UserMapper.java
@@ -3,7 +3,11 @@ package ks45team03.rentravel.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import ks45team03.rentravel.dto.User;
+
 @Mapper
 public interface UserMapper {
+	
+	public User checkPwByUserId(String userId);
 
 }
