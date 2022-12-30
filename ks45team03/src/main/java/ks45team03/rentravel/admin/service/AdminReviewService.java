ffef555e79750f9ff34f947b9f8f61ac420e11dc
ASUS <ASUS@DESKTOP-T50VO8O>
2022-12-30 11:10:44 +0900

    [221229 남윤희] admin 쪽 layout default 수정 default.html 헤더랑 풋 부분에 datatable.js 추가

--- ks45team03/src/main/java/ks45team03/rentravel/admin/service/AdminReviewService.java
+++ ks45team03/src/main/java/ks45team03/rentravel/admin/service/AdminReviewService.java
@@ -1,8 +1,25 @@
 package ks45team03.rentravel.admin.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import ks45team03.rentravel.dto.Review;
+import ks45team03.rentravel.mapper.AdminReviewMapper;
 
 @Service
 public class AdminReviewService {
-
+	
+	private final AdminReviewMapper adminReviewMapper;
+	
+	public AdminReviewService(AdminReviewMapper adminReviewMapper) {
+		this.adminReviewMapper = adminReviewMapper;
+	}
+	
+	public List<Review> getAdminReviewList(){
+		
+		List<Review> adminReviewList = adminReviewMapper.getAdminReviewList();
+		
+		return adminReviewList;
+	}
 }
