748d8914b8d4c40fae12ea927c9a0ef1f7a76fef
ksmart <ksmart@DESKTOP-LIJ9H19>
2022-12-30 17:30:04 +0900

    [221230 신지혜] 결제취소 적립금내역 수정 - 적립금 컨트롤러, 서비스, 매퍼에 mileageRentalCancelSaveList 추가함 - 근데 수정 필요함

--- ks45team03/src/main/java/ks45team03/rentravel/mapper/AdminMileageMapper.java
+++ ks45team03/src/main/java/ks45team03/rentravel/mapper/AdminMileageMapper.java
@@ -5,12 +5,16 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import ks45team03.rentravel.dto.MileagePaymentSave;
+import ks45team03.rentravel.dto.MileageRentalCancelSave;
 
 @Mapper
 public interface AdminMileageMapper {
 
 	//결제적립금 조회
 	public List<MileagePaymentSave> adminGetMileagePaymentSaveList();
+	
+	//취소적립금 조회
+	public List<MileageRentalCancelSave> adminGetMileageRentalCancelSaveList();
 
 
 
