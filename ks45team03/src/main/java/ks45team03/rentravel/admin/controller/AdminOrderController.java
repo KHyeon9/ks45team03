9201504f95190217f9bd211910e9ae49483c95a0
ksmart <ksmart@DESKTOP-LIJ9H19>
2023-01-02 16:35:20 +0900

    [230102 강현구] 관리자 주문 내역 수정 작업 관리자 주문 내역 수정 작업

--- ks45team03/src/main/java/ks45team03/rentravel/admin/controller/AdminOrderController.java
+++ ks45team03/src/main/java/ks45team03/rentravel/admin/controller/AdminOrderController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,15 +16,18 @@ import ks45team03.rentravel.dto.RentalCancel;
 import ks45team03.rentravel.dto.Return;
 import ks45team03.rentravel.dto.WaybillOwner;
 import ks45team03.rentravel.dto.WaybillRenter;
+import ks45team03.rentravel.mapper.AdminOrderMapper;
 
 @Controller
 @RequestMapping("/admin/order")
 public class AdminOrderController {
 	
 	private final AdminOrderService adminOrderService;
+	private final AdminOrderMapper adminOrderMapper;
 	
-	public AdminOrderController(AdminOrderService adminOrderService) {
+	public AdminOrderController(AdminOrderService adminOrderService, AdminOrderMapper adminOrderMapper) {
 		this.adminOrderService = adminOrderService;
+		this.adminOrderMapper = adminOrderMapper;
 	}
 	
 	//주문 내역 리스트 조회
@@ -54,9 +58,25 @@ public class AdminOrderController {
 		return "admin/order/adminOrderHistory";
 	}
 	
+	// 관리자 주문 수정 액션
+	@PostMapping("/adminOrderModify")
+	public String adminOrderModify(Rental rentalInfo) {
+		
+		adminOrderService.modifyOrderInfo(rentalInfo);
+		
+		return "redirect:/admin/order/adminOrderHistory";
+	}
+	
+	// 관리자 주문 수정 폼 정보 조회
 	@GetMapping("/adminOrderModify")
-	public String adminOrderModify(Model model) {
+	public String adminOrderModify(@RequestParam(value = "rentalCode") String rentalCode,
+									Model model) {
+		System.out.println(rentalCode);
+		
+		Rental orderInfo = adminOrderMapper.getOrderInfoByRentalCode(rentalCode);
+		
 		model.addAttribute("title","관리자 주문 수정");
+		model.addAttribute("orderInfo", orderInfo);
 		return "admin/order/adminOrderModify";
 	}
 	
@@ -81,7 +101,7 @@ public class AdminOrderController {
 	// 주문 취소 내역 조회 
 	@SuppressWarnings("unchecked")
 	@GetMapping("/adminRentalcancelList")
-	public String adminRentalcancelList(@RequestParam(value = "int currentPage", defaultValue = "1", required = false) int currentPage,
+	public String adminRentalcancelList(@RequestParam(value = "currentPage", defaultValue = "1", required = false) int currentPage,
 										Model model) {
 		Map<String, Object> paramMap = adminOrderService.getRentalCancelHistory(currentPage);
 		
@@ -123,8 +143,10 @@ public class AdminOrderController {
 	}
 	
 	@GetMapping("/adminReturnModify")
-	public String adminReturnModify(Model model) {
+	public String adminReturnModify(@RequestParam(value="rentalCode") String rentalCode, 
+									Model model) {
 		model.addAttribute("title","관리자 환불 수정");
+		
 		return "admin/order/adminReturnModify";
 	}
 }
