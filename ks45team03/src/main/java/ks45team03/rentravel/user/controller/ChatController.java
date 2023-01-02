9cd62f2a67ca36d8cff5e575a13a91ac37b67c68
ASUS <ASUS@DESKTOP-T50VO8O>
2023-01-02 17:29:27 +0900

    [230102 남윤희] chatting 구현중, websocket 채팅보내기 까지 채팅 보내기 까지 하고 stomp 적용 직전

--- ks45team03/src/main/java/ks45team03/rentravel/user/controller/ChatController.java
+++ ks45team03/src/main/java/ks45team03/rentravel/user/controller/ChatController.java
@@ -1,23 +1,31 @@
 package ks45team03.rentravel.user.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import ks45team03.rentravel.dto.ChatRoom;
+import ks45team03.rentravel.user.service.ChatService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
 @RequestMapping("/chat")
 public class ChatController {
-	
-	@GetMapping("/chatRoomList")
-	public String reviewList(Model model) {
-		model.addAttribute("title","채팅방 목록 화면");
-		return "user/chat/chatRoomList";
-	}
-	
-	@GetMapping("/inChat")
-	public String inChat(Model model) {
-		model.addAttribute("title","채팅방 내부");
-		return "user/chat/inChat";
-	}
+
+    private final ChatService chatService;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom() {
+        return chatService.findAllRoom();
+    }
 }
