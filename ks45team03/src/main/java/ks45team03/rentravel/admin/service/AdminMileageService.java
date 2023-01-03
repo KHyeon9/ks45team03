875a6464940e68228f8704efb064b8db1bce7e5d
ksmart <ksmart@DESKTOP-LIJ9H19>
2023-01-03 14:31:05 +0900

    [230103 신지혜] 관리자화면 적립금 검색 가능 - 관리자 적립금 화면에서 특정 회원의 적립금 검색 가능

--- ks45team03/src/main/java/ks45team03/rentravel/admin/service/AdminMileageService.java
+++ ks45team03/src/main/java/ks45team03/rentravel/admin/service/AdminMileageService.java
@@ -21,29 +21,17 @@ public class AdminMileageService {
 	}
 
 	/**
-	 * 모든 회원의 전체 적립금 내역 조회
+	 * 회원의 적립금 내역 조회
 	 * @param searchValue 
 	 * @param searchKey 
 	 * @return
 	 */
 	public List<MileageSaveUse> adminGetMileageList(String searchKey, String searchValue){
-		List<MileageSaveUse> mileageList = adminMileageMapper.adminGetMileageList();
 		
+		List<MileageSaveUse> mileageList = adminMileageMapper.adminGetMileageList(searchKey, searchValue);
 		
 		return mileageList;
 	}
 	
-	/**
-	 * 특정 회원의 전체 적립금 내역 조회
-	 * @param userId
-	 * @return
-	 */
-	public MileageSaveUse getMileageById(String userId) {
-		
-		MileageSaveUse userMileage = adminMileageMapper.getMileageById(userId);
-		
-		return userMileage;
-		
-		
-	}
+	
 }
