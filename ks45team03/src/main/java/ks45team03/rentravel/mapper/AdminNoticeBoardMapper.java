70e4ef66ad7a2ab471ffcb589a20f6d034b89c6c
toroy <toroy@DESKTOP-32BBT50>
2023-01-03 23:26:39 +0900

    [230103 김대현] 관리자 공지사항 CRUD 작성완료 추후 보완점 등록, 수정시 유효성검사 등록시 적용하는 스타일이나 공백, 칸적용이 안되고 한줄로 나오는점

---
+++ ks45team03/src/main/java/ks45team03/rentravel/mapper/AdminNoticeBoardMapper.java
@@ -0,0 +1,30 @@
+package ks45team03.rentravel.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import ks45team03.rentravel.dto.NoticeBoard;
+
+@Mapper
+public interface AdminNoticeBoardMapper {
+
+	// 공지사항 리스트
+	public List<NoticeBoard> adminNoticeBoardList();
+	
+	// 공지사항 상세보기
+	public NoticeBoard adminDetailNoticeBoard(String noticeBoardCode);
+	
+	// 공지사항 등록
+	public int adminAddNoticeBoard(NoticeBoard noticeBoard);
+	
+	// 공지사항 수정화면
+	public NoticeBoard modifyNoticeBoard(String noticeBoardCode);
+	
+	// 공지사항 수정처리
+	public int modifyNoticeBoardAction(NoticeBoard noticeBoard);
+	
+	// 공지사항 삭제
+	public int removeNoticeBoard(String noticeBoardCode);
+
+}
