b974c17351eb5a1bd8c54f5769cfeedceb8239cc
ECS <ECS@DESKTOP-048HU3O>
2023-01-05 15:42:37 +0900

    [230105 오경하] 1대1 문의 게시판 답글 구현 <user> 1. 1대1 문의 게시판 답글 구현 2. 1대1 문의 게시판 답글 조회

--- ks45team03/src/main/java/ks45team03/rentravel/mapper/InquiryBoardMapper.java
+++ ks45team03/src/main/java/ks45team03/rentravel/mapper/InquiryBoardMapper.java
@@ -9,8 +9,14 @@ import ks45team03.rentravel.dto.InquiryBoard;
 @Mapper
 public interface InquiryBoardMapper {
 	
+	/* 1 대 1 문의게시판 답변 게시글 조회 */
+	public InquiryBoard getInquiryReBoard(String inquiryReBoardCode);
+	
 	/* 1 대 1 문의게시판 게시글 상세 조회 */
 	public InquiryBoard getInquiryBoard(String inquiryBoardCode);
+	
+	/*  1 대 1 문의게시판 답글목록 조회 */ 
+	public List<InquiryBoard> getInquiryBoardReList ();
 	
 	/* 1 대 1 문의게시판 목록 조회 */
 	public List<InquiryBoard> getInquiryBoardList (int startIndex, int pageSize);
