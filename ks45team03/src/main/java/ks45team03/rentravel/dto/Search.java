b0a5903738f5a8af7cda161db2ff93284173e5d6
toroy <toroy@DESKTOP-32BBT50>
2023-01-16 16:29:12 +0900

    [230116 김대현]공지사항, FAQ 수정 및 관리자 페이지 관리자 아닐때 접근제한, 관리자 메인페이지 세션추가 관리자 공지사항 html 부트스트랩 변경 관리자 공지사항 컨트롤러, 매퍼, 매퍼.xml에 페이징과 검색기능 추가 관리자 공지사항, FAQ 페이징 현재페이지 색상과 이전, 이후 사용하지 못할 때 색상 변경 관리자 공지사항, FAQ 리스트 너비 조절 관리자 페이지 우측상단 닉네임 삽입 관리자페이지 메인에서 삭제 후 세션아이디와 로그인레벨이 관리자가 아니면 로그인창으로 리다이렉트 만약의 사태를 대비해 로그인했는데 관리자가아니면 홈화면으로 리다이렉트 DTO Search 추가 관리자  FAQ 검색 후 페이징할 때 페이징 풀리는 오류 수정(컨트롤러에 search 생성자 생성 후 html에 뿌려줌) 관리자 공지사항 검색 후 페이징 할 때 페이징 풀리는 오류 수정(컨트롤러에 search 생성자 생성 후 html에 뿌려줌)

--- ks45team03/src/main/java/ks45team03/rentravel/dto/Search.java
+++ ks45team03/src/main/java/ks45team03/rentravel/dto/Search.java
@@ -8,6 +8,12 @@ import lombok.ToString;
 @Setter
 @ToString
 public class Search {
+	
+	 public Search(String searchKey, String searchValue) { 
+		 this.searchKey = searchKey; 
+		 this.searchValue = searchValue; 
+		 }
+	 
 	private String searchKey;
 	private String searchValue;
 }
